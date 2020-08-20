dd04d414f7b0e945e394d743e2d1ca42bb0e5f31
Henry Liu <hl3184@columbia.edu>
2020-08-20 18:37:11 -0400

    server set up complete

--- models/Workout.js
+++ models/Workout.js
@@ -2,17 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  day: { type: Date },
-  exercises: [
-    {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
-    },
-  ],
+  day: { type: Date, default: Date.now },
+  exercises: [],
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+const db = require('./models');
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +16,39 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', {
   useNewUrlParser: true,
 });
 
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+app.get('/api/workouts', async (req, res) => {
+  const arr = await db.Workout.find();
+  res.json(arr);
+});
+
+app.get('/api/workouts/range', async (req, res) => {
+  const arr = await db.Workout.find();
+  res.json(arr);
+});
+
+app.post('/api/workouts', async (req, res) => {
+  const data = await db.Workout.create(req.body);
+  res.json(data);
+});
+
+app.put('/api/workouts/:id', async (req, res) => {
+  const data = await db.Workout.updateOne(
+    {
+      _id: mongoose.Types.ObjectId(req.params.id),
+    },
+    { $push: { exercises: req.body } }
+  );
+
+  res.json(data);
+});
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
