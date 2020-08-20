35a1dada12997ef7aa192f235dbc003f24ab7307
Henry Liu <hl3184@columbia.edu>
2020-08-20 18:55:10 -0400

    modify workout modal

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
   day: { type: Date, default: Date.now },
-  exercises: [],
+  exercises: [
+    {
+      type: { type: String },
+      name: { type: String },
+      distance: { type: Number },
+      duration: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
+    },
+  ],
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
